57678e0c069cd9f906e2485f9de99d0138728265
Ram Madhav Bommaraboyina <131741895+rammadhav929@users.noreply.github.com>
2026-01-20 23:04:07 +0530

    Update access token and phone number ID

--- whatsapp_bot/send.py
+++ whatsapp_bot/send.py
@@ -1,8 +1,8 @@
 import requests
 
 VERIFY_TOKEN = "my_verify_token"
-ACCESS_TOKEN = "ZA"
-PHONE_NUMBER_ID = ""
+ACCESS_TOKEN = "EAAQHhYsOVzsBQkA5P2MKQTa9l678TdgJ48n2lrPSKcs7dLY1gXuerWOTs9OuwAlnLZCFXNgBYuth8epZB5W6tBjuaJn3fa5NRgsIFbhObcOnZAwqD8FrH4meXUkYIL81fKqt1ZCUUX6ruDVMx5wFeeYZB2ExkRDxAjGeG8QgOW1CnA0nmhAAcB0tc0fY2Tt6GNafhxjXZCEa2Vt8OsiX6ZAtPnRY8uYnGTZBHPtxZBasVXrZBirObxZAw7eDZCeGIwVse3kGuKJSZBEuZB2gn3eZCPJ9ZB0qZAWynQgZDZD"
+PHONE_NUMBER_ID = "894423013757849"
 BOT_URL = "http://127.0.0.1:8000/reply"  # <-- Put your bot endpoint here
 
 def send_whatsapp_message(to, text, use_template=False):
