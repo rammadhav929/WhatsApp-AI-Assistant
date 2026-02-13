c7eeaeaf3ccab95ecc741803d0a020bc721be10d
Ram Madhav Bommaraboyina <131741895+rammadhav929@users.noreply.github.com>
2026-02-13 15:39:10 +0530

    Update sending.py

--- whatsapp_bot/sending.py
+++ whatsapp_bot/sending.py
@@ -1,7 +1,7 @@
 import requests
 
 def trigger(text):
-    url = "http://172.19.128.1:8000/api/text/"  # adjust path if needed
+    url = "http://10.6.21.30:8000/api/text/"  # adjust path if needed
     
     response = requests.post(
         url,
@@ -12,4 +12,5 @@ def trigger(text):
     response.raise_for_status()
     res=response.json()
     return res['result']
+
 
