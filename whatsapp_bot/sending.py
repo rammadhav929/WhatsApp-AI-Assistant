c507aa848d8b970232b3400549ce7b3bc2e27a97
Ram Madhav Bommaraboyina <131741895+rammadhav929@users.noreply.github.com>
2026-02-13 14:26:26 +0530

    Update sending.py

--- whatsapp_bot/sending.py
+++ whatsapp_bot/sending.py
@@ -1,13 +1,14 @@
 import requests
 
-def trigger_text_api(text):
-    url = "http://127.0.0.1:8000/proxy_text_api/"  # adjust path if needed
+def trigger(text):
+    url = "http://127.0.0.1:8000/api/text/"  # adjust path if needed
     
     response = requests.post(
         url,
         json={"text": text},
-        timeout=30
+        timeout=150
     )
 
     response.raise_for_status()
-    return response.json()
+    res=response.json()
+    return res['result']
